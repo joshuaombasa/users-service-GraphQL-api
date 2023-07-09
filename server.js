d61ee4504953c23fad5949adc5538190fd297f69
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-09 16:57:26 +0300

    updated server.js to include subscriptions

--- server.js
+++ server.js
@@ -1,96 +1,122 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const { execute, subscribe } = require('graphql');
+const { createServer } = require('http');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+const { PubSub } = require('graphql-subscriptions');
 const mysql = require('mysql2');
 
-// Create a MySQL connection
+const pubsub = new PubSub();
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'cussons',
-  });
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'cussons',
+});
 
-
-// Define a GraphQL schema
 const schema = buildSchema(`
-type User {
-  id: ID!
-  name: String!
-  email: String!
-}
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
 
-type Query {
-  user(id: ID!): User
-}
+  type Query {
+    user(id: ID!): User
+    users: [User]
+  }
 
-type Mutation {
+  type Mutation {
     createUser(name: String!, email: String!): User
     deleteUser(id: ID!): String
   }
+
+  type Subscription {
+    userAdded: User
+  }
 `);
 
-
-// Define resolvers
 const root = {
-    user: ({ id }) => {
-      // Retrieve a user from the database by ID
-      return new Promise((resolve, reject) => {
-        connection.query('SELECT * FROM users WHERE id = ?', [id], (error, results) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(results[0]);
-          }
-        });
+  user: ({ id }) => {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM users WHERE id = ?', [id], (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results[0]);
+        }
       });
-    },
-    createUser: ({ name, email }) => {
-      // Insert a new user into the database
-      const user = { name, email };
-      return new Promise((resolve, reject) => {
-        connection.query('INSERT INTO users SET ?', user, (error, results) => {
-          if (error) {
-            reject(error);
-          } else {
-            user.id = results.insertId;
-            resolve(user);
-          }
-        });
+    });
+  },
+  users: () => {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
       });
-    },
-    deleteUser: ({ id }) => {
-        return new Promise((resolve, reject) => {
-          connection.query('DELETE FROM users WHERE id = ?', [id], (error, results) => {
-            if (error) {
-              reject(error);
-            } else {
-              if (results.affectedRows > 0) {
-                resolve(`User with ID ${id} deleted successfully`);
-              } else {
-                reject(new Error(`User with ID ${id} not found`));
-              }
-            }
-          });
-        });
-      }
-  };
+    });
+  },
+  createUser: ({ name, email }) => {
+    const user = { name, email };
+    return new Promise((resolve, reject) => {
+      connection.query('INSERT INTO users SET ?', user, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          user.id = results.insertId;
+          pubsub.publish('userAdded', { userAdded: user });
+          resolve(user);
+        }
+      });
+    });
+  },
+  deleteUser: ({ id }) => {
+    return new Promise((resolve, reject) => {
+      connection.query('DELETE FROM users WHERE id = ?', [id], (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          if (results.affectedRows > 0) {
+            resolve(`User with ID ${id} deleted successfully`);
+          } else {
+            reject(new Error(`User with ID ${id} not found`));
+          }
+        }
+      });
+    });
+  },
+};
 
-
- // Create an Express app
 const app = express();
 
-// Define a GraphQL endpoint
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
     rootValue: root,
-    graphiql: true, // Enable GraphiQL interface for testing
+    graphiql: true,
   })
 );
 
-// Start the server
-app.listen(3000, () => {
+const server = createServer(app);
+server.listen(3000, () => {
   console.log('Server started on http://localhost:3000/graphql');
 });
+
+const subscriptionServer = SubscriptionServer.create(
+  {
+    execute,
+    subscribe,
+    schema,
+  },
+  {
+    server: server,
+    path: '/subscriptions',
+  },
+  (ws, req) => console.log('WebSocket connected')
+);
